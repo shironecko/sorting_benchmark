bb668c1ae9a00396fd34607bbffd9605c81e97aa
Shironecko <shironecko.mail@gmail.com>
2014-08-17 18:01:31 +0300

    Implemented integrity test.

--- source/benchmarking/test.cpp
+++ source/benchmarking/test.cpp
@@ -1,9 +1,22 @@
-#include <map>
+#include <unordered_map>
 #include "test.h"
 
 bool integrity_test(int *unsorted, int *sorted, unsigned int len)
 {
-	return false;
+	std::unordered_map<unsigned int, int> counts;
+	for (unsigned int i = 0; i < len; i++)
+	{
+		counts[unsorted[i]]++;
+		counts[sorted[i]]--;
+	}
+
+	for each (auto count in counts)
+	{
+		if (count.second != 0)
+			return false;
+	}
+
+	return true;
 }
 
 bool sort_test(int *arr, unsigned int len)
