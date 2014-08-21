341505d6010476cb8208638de2f8ae4048c45927
Shironecko <shironecko.mail@gmail.com>
2014-08-21 05:25:45 +0300

    Updated main according to recent changes.

--- source/benchmarking/main.cpp
+++ source/benchmarking/main.cpp
@@ -1,26 +1,22 @@
 #include <iostream>
 #include <iomanip>
-#include <chrono>
 #include <thread>
 #include <string>
 #include <map>
+#include <vector>
 #include <boost/program_options.hpp>
 
-#include "options.h"
-#include "benchmark.h"
 #include "time_helper.h"
+#include "options.h"
+#include "pipeline_data.h"
+#include "array_prepare.h"
+#include "benchmark.h"
 
+using namespace std;
+using namespace time_helper;
 namespace opt = boost::program_options;
-namespace chrono = std::chrono;
-namespace th = time_helper;
-using std::cout;
-using std::cerr;
-using std::endl;
-using std::setw;
 
 options g_options;
-
-void print_time(std::chrono::duration<std::chrono::high_resolution_clock::rep, std::chrono::high_resolution_clock::period> time);
 
 int main(int argc, char* argv[])
 {
@@ -47,12 +43,12 @@ int main(int argc, char* argv[])
 	}
 
 	// benchmark map
-	std::map<std::string, void(*)(int *arr, unsigned int len, const options& opt, benchmark_results& result)> benchmark_map;
-	benchmark_map["bubble"] = bubble_benchmark;
-	benchmark_map["insertion"] = insertion_benchmark;
-	benchmark_map["merge"] = merge_benchmark;
+	std::map<std::string, void(*)(pipeline_data& data)> benchmark_map;
+	benchmark_map["bubble"] = benchmark_bubble;
+	benchmark_map["insertion"] = benchmark_insertion;
+	benchmark_map["merge"] = benchmark_merge;
 
-	// runing benchmark
+	// checking if the user specified a valid algorithm for benchmarking
 	auto benchmark = benchmark_map[g_options.algorithm];
 	if (benchmark == nullptr)
 	{
@@ -62,49 +58,57 @@ int main(int argc, char* argv[])
 
 	cout << "Algorithm : " << g_options.algorithm << endl << endl;
 
-	benchmark_results result;
-	std::chrono::duration<std::chrono::high_resolution_clock::rep, std::chrono::high_resolution_clock::period> total_time, min_time, max_time;
+	// prepairing pipeline
+	vector<void(*)(pipeline_data& data)> pipeline;
+	pipeline.push_back(prepare_array);
+	pipeline.push_back(benchmark);
+
+	// prepairing pipeline data
+	pipeline_data data{};
+	data.array_size = g_options.array_size;
+	data.unsorted_array = new int[data.array_size];
+	data.sorted_array = new int[data.array_size];
+	data.min_number = g_options.arr_min_num;
+	data.max_number = g_options.arr_max_num;
+
+	// misc statistical vars
+	duration total_time, min_time, max_time;
 	unsigned int integrity = 0, sorted = 0;
 
+	// the pipeline itself
 	for (unsigned int i = 0; i < g_options.iterations; i++)
 	{
-		benchmark_wrap(g_options, result, benchmark);
-		total_time += result.time_taken;
-		if (result.test_integrity) integrity++;
-		if (result.test_sorted) sorted++;
+		for (auto step : pipeline)
+			step(data);
 
-		if (result.time_taken < min_time || min_time == min_time.zero())
-			min_time = result.time_taken;
-		if (result.time_taken > max_time)
-			max_time = result.time_taken;
+		total_time += data.time_taken;
+		if (data.test_integrity) integrity++;
+		if (data.test_sorted) sorted++;
 
-		if (g_options.verbosity_level >= 1)
+		if (data.time_taken < min_time || min_time == min_time.zero())
+			min_time = data.time_taken;
+		if (data.time_taken > max_time)
+			max_time = data.time_taken;
+
+		if (g_options.verbosity_level >= 2)
 			cout << "Time taken: ";
 
-		if (g_options.verbosity_level >= 0)
-			cout << chrono::duration_cast<chrono::seconds>(total_time).count() << ":"
-				<< chrono::duration_cast<chrono::milliseconds>(total_time).count() % 1000 << endl << endl;
+		if (g_options.verbosity_level >= 1)
+			cout << to_string(data.time_taken) << endl;
 	}
+
+	// clearing data
+	delete[] data.unsorted_array;
+	delete[] data.sorted_array;
 
 	if (g_options.test)
 	{
-		cout << "Integrity : " << setw(number_width(g_options.iterations)) << integrity << "/" << g_options.iterations << endl;
-		cout << "Sorted    : " << setw(number_width(g_options.iterations)) << sorted << "/" << g_options.iterations << endl;
+		cout << "Integrity : " << integrity << "/" << g_options.iterations << endl;
+		cout << "Sorted    : " << sorted << "/" << g_options.iterations << endl;
 	}
 
-	cout << "Min time     : " << th::to_string(min_time) << " | ";
-	print_time(min_time);
-	cout << "Max time     : " << th::to_string(max_time) << " | ";
-	print_time(max_time);
-	cout << "Average time : " << th::to_string(total_time / g_options.iterations) << " | ";
-	print_time(total_time / g_options.iterations);
-	cout << "Total time   : " << th::to_string(total_time) << " | ";
-	print_time(total_time);
-}
-
-void print_time(std::chrono::duration<std::chrono::high_resolution_clock::rep, std::chrono::high_resolution_clock::period> time)
-{
-	cout << setw(3) << chrono::duration_cast<chrono::seconds>(time).count() << ":"
-		<< setw(3) << chrono::duration_cast<chrono::milliseconds>(time).count() % 1000 << ":"
-		<< setw(3) << chrono::duration_cast<chrono::microseconds>(time).count() % 1000 << endl;
+	cout << "Min time     : " << to_string(min_time) << endl;
+	cout << "Max time     : " << to_string(max_time) << endl;
+	cout << "Average time : " << to_string(total_time / g_options.iterations) << endl;
+	cout << "Total time   : " << to_string(total_time) << endl;
 }
